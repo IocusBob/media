b03dfa0a92766a6d063567feebacb41d2917b47e
Rob <bobs-n@hotmail.com>
2022-12-08 12:54:06 +0000

    Initial setup of redux and thunk

---
+++ src/components/UsersList.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchUsers } from '../store'
+
+const UsersList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [])
+
+  return (
+    <div>UsersList</div>
+  )
+}
+
+export default UsersList
