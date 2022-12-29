a0f4bdefc00eed8eba838f5cd528f533aa0449a2
Rob <bobs-n@hotmail.com>
2022-12-29 14:24:12 +0000

    Improved logic/rendering

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -19,28 +19,27 @@ const UsersList = () => {
     doFetchUsers();
   }, [doFetchUsers]);
 
+  let content;
   if (isLoadingUsers) {
-    return <Skeleton times={6} extraClasses="h-10 w-full"/>
-  }
-  
-  if(loadingUsersError){
-    return <div>Error fetching data...</div>
-  }
-
-  const renderedUsers = data.map(user => {
-    return <div key={user.id} className="mb-2 border rounded">
-      <div className='flex p-2 justify-between items-center cursor-pointer'>
-        {user.name}
+    content = <Skeleton times={6} extraClasses="h-10 w-full"/>
+  } else if(loadingUsersError){
+    content = <div>Error fetching data...</div>
+  } else {
+    content = data.map(user => {
+      return <div key={user.id} className="mb-2 border rounded">
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+          {user.name}
+        </div>
       </div>
-    </div>
-  })
+    })
+  }
 
   const handleUserAdd = () => {
     doCreateUser();
   };
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
           <Button loading={isCreateingUser} onClick={handleUserAdd}> + Add User</Button>
         {
@@ -48,7 +47,7 @@ const UsersList = () => {
         }
         
       </div>
-      {renderedUsers}
+      {content}
     </div>
   )
 }
